2bb5e62659bc887f46534e3a45d16510090a7806
Alex Huillca <alexhuillcamejia123@gmail.com>
2024-09-27 20:45:54 -0500

    query prendasDisponibles

--- src/main/java/com/example/lookup/repository/PrendaTiendaRepository.java
+++ src/main/java/com/example/lookup/repository/PrendaTiendaRepository.java
@@ -1,10 +1,16 @@
 package com.example.lookup.repository;
 
+import com.example.lookup.dtos.PrendasDisponiblesDTO;
 import com.example.lookup.entities.PrendaTienda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PrendaTiendaRepository extends JpaRepository<PrendaTienda,Long>
 {
+    @Query("select new com.example.lookup.dtos.PrendasDisponiblesDTO(pt.prendaIdPrenda.nombrePrenda,pt.idTienda.nombreTienda,pt.stockDisponible,pt.prendaIdPrenda.precioFinal) from PrendaTienda pt where pt.disponible= true order by pt.stockDisponible")
+    public List<PrendasDisponiblesDTO> prendasDisponibles();
 }
